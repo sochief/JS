2ce911e6f1c67b1c91b4592769784a6274d22884
Chief <shafkatoo@gmail.com>
2019-10-01 20:38:02 +0300

    01.10

---
+++ script.js
@@ -0,0 +1,32 @@
+/*console.log('Hello World!!!');
+var firstName = 'John';
+console.log(firstName);//you will see the name in the console, coz we load directly into the console itself
+var lastName = 'Smith';
+var age = 28;
+console.log(firstName + lastName + age );
+// JS has a dynamic typing: data types are automaticly assgned to variables
+var fullAge = true;
+console.log(fullAge);
+var job;
+console.log(job)
+job = 'Teacher';
+console.log(job)
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + " " + age);
+// define at the same time, define in the other
+ var job, isMarried;
+ jod = 'teacher';
+ isMarried = false;
+ console.log(job + isMarried + '\n' + 'Yeah, false')
+ //Variable notation
+ age = 'twenty eight';
+ // it's automaticaly figures out the last type, so no worries!
+ alert('Finally, Ive learned something new');
+var lastName = prompt('What is his last name? ');
+alert('Last name has changed! ');
+console.log(firstName + " " + lastName);
+*************************************************************/
+//******************Basic operators */
